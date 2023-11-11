67762e926d7f17087d22b068713fc0decff4f8d3
Cleyton Bezerra Miguel <116188906+CleytonDev100@users.noreply.github.com>
2023-11-11 09:44:58 -0300

    v4

--- src/componentes/camera/camera.jsx
+++ src/componentes/camera/camera.jsx
@@ -6,36 +6,64 @@ import { useNavigate } from "react-router-dom";
 export default function Camera() {
   const webcamRef = React.useRef(null);
   const [countdown, setCountdown] = useState(10);
+  const [preDetect, setpreDetect] = useState("");
+  const [cont, setCont] = useState(4)
   const navigate = useNavigate();
 
   useEffect(() => {
+    Pre_detect();
     const timer = setInterval(() => {
       if (countdown > 0) {
-        setCountdown(countdown - 1);
-      } else {
+        setCountdown(countdown - 1)
+      } 
+      else {
         clearInterval(timer);
-        capture();
+        Captura();
       }
     }, 1000);
-
     return () => clearInterval(timer);
   }, [countdown]);
 
+  async function Pre_detect(){
+    const imageSrc = webcamRef.current.getScreenshot();
+    if (imageSrc) {
+      try {
+        const blob = await fetch(imageSrc).then(r => r.blob());
+        const Pre_formdata = new FormData();
+        Pre_formdata.append('imagem', blob, 'imagem.png');
+        Pre_formdata.append('email', localStorage.getItem("email_aluno"));
+        const response = await axios.post("https://sampa.pythonanywhere.com/pre_detectimg/", Pre_formdata)
+        setpreDetect(response.data.pre_deteccao_resultado)
+        if(countdown === 1 && response.data.pre_deteccao_resultado === "Não estamos conseguindo te detectar"){
+          setCountdown(5)
+          setCont(cont - 1)
+        }
+        else if(cont === 0){
+          Captura();
+        }
+      }
+      catch (error) {
+        alert(error)
+      }
+      finally{
+        setTimeout(Pre_detect, 15000);
+      }
+    }
+  }
 
-  async function capture() {
+  async function Captura() {
     if (webcamRef.current) {
       const imageSrc = webcamRef.current.getScreenshot();
 
       if (imageSrc) {
         try {
           const blob = await fetch(imageSrc).then(r => r.blob());
-
           const formData = new FormData();
           formData.append('imagem', blob, 'imagem.png');
           formData.append('email', localStorage.getItem("email_aluno"))
 
           try{
-            const response = await axios.post("http://127.0.0.1:8000/api_salvarimg/", formData)
+            const response = await axios.post("https://sampa.pythonanywhere.com/api_salvarimg/", formData)
             const resultado = response.data.deteccao_resultado
 
             if (resultado){
@@ -70,8 +98,7 @@ export default function Camera() {
 
   return (
     <>
-
-
+      <h1 className="text-[30px] ml-[35%] xl:ml-[40%] xl:text-[40px]">{preDetect}</h1>
       <h1 className="text-[30px] ml-[35%] xl:ml-[40%] xl:text-[40px]">Bem-Vindo ao
         <span className="text-red-600">Teste</span></h1>
       <h1 className="xl:text-[25px] text-[20px] mt-[10px] ml-[45%] text-red-600 w-[120px] items-center xl:text- xl:w-[220px]">INSTRUÇÕES</h1>
